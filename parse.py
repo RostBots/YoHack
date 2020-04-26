cdc04a515816b2ae0e490145a870f49f8318a134
dprozz <dmitri.proskuriakov@gmail.com>
2020-04-26 14:43:41 +0300

    parser fixed

--- parse.py
+++ parse.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 def get_answers(text):
-    i = 6
+    i = data.shape[0]
     for kp in range(10):
-        link = f'https://stackoverflow.com/search?page={kp + 1}&pagesize=50&q={"+".join(text.split())}&sort=answers'
+        link = f'https://stackoverflow.com/search?q={"+".join(text.split())}&sort=answers'
         answer = requests.get(link)
 
         soup = BeautifulSoup(answer.text, 'html.parser')
@@ -18,11 +18,8 @@ def get_answers(text):
             try:
                 questions.append(elem.text)
                 answers.append(" ".join(BeautifulSoup(requests.get('https://stackoverflow.com' + elem['href']).text, 'html.parser').find_all('div', {'class': 'answer'})[0].text.split()))
-
-                print(1)
                 data.loc[i] = [questions[-1], answers[-1]]
                 i += 1
-                print(2)
             except Exception as e:
                 print(e)
 
